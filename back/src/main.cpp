e47605fe8596b60907bfb61b5c937f6656b6f875
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2026-04-15 08:55:32 +0000

    build: Allow changing port and binary name ```sh make NAME=gomoku_9013 PORT=9013 ```

--- back/src/main.cpp
+++ back/src/main.cpp
@@ -3,13 +3,16 @@
 #include "Server.hpp"
 #include "Routes.hpp"
 
+#ifndef PORT
+#define PORT 9012
+#endif
+
 int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	std::cout << "hello Server!" << std::endl;
 
-	Server server(9012);
+	Server server(PORT);
 
 	GameState gs;
 	registerRoutes(server, gs);
