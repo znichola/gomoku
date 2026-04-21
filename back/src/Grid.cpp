0257c73c99c93d3c0785c738a5178f55d17cdc39
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2026-04-21 16:25:54 +0200

    comment

--- back/src/Grid.cpp
+++ back/src/Grid.cpp
@@ -131,24 +131,24 @@ Cell Grid::getWinningLineColor() const {
      * PHASE 1 (on a cell == NULL, car première fois parcouru)
      * step = 0
      *(0). . .  <-- X, O, .         def:   NODE.type = N0
-        * . 1 . .  <-- X, O            verif: N1 != . et N0 != N1 (next)
-        * . . 2 .
-        * . . . 3
-        * 
-        * PHASE 2 (on a cell != NULL et step = 1)
-        * step = 1 LOOKING
-        * 0 . . .
-        * .(1). .                      verif: N1 == N2 (next)
-        * . . 2 .  <-- X, O
-        * . . . 3
-        * 
-        * PHASE 3
-        * step = 2 BOILING
-        * 0 . . .
-        * . 1 . .                      verif: N2 != N3 && NODE.type != N3 (next) 
-        * . .(2).  <-- X, O 
-        * . . . 3
-        */
+     * . 1 . .  <-- X, O            verif: N1 != . et N0 != N1 (next)
+     * . . 2 .
+     * . . . 3
+     * 
+     * PHASE 2 (on a cell != NULL et step = 1)
+     * step = 1 LOOKING
+     * 0 . . .
+     * .(1). .                      verif: N1 == N2 (next)
+     * . . 2 .  <-- X, O
+     * . . . 3
+     * 
+     * PHASE 3
+     * step = 2 BOILING
+     * 0 . . .
+     * . 1 . .                      verif: N2 != N3 && NODE.type != N3 (next) 
+     * . .(2).  <-- X, O 
+     * . . . 3
+     */
 
     const int dmax = d * d;
     for (int id = 0; id < dmax; ++id) {
